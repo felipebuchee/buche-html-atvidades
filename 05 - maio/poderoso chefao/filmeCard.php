ecf0032603ed5ebf0c20e6209a1e96ae397eab55
Felipe Buche <felipe.buche.tds2023@gmail.com>
2025-05-30 13:29:42 -0300

    .

--- 05 - maio/poderoso chefao/filmeCard.php
+++ 05 - maio/poderoso chefao/filmeCard.php
@@ -5,6 +5,7 @@ require_once('model/Filme.php');
 $titulo = $_POST['titulo'];
 $ano = $_POST['ano'];
 $diretor = $_POST['diretor'];
+
 $imagem = $_POST['imagem'];
 
 $filme = new Filme($titulo, $ano, $diretor, $imagem);
